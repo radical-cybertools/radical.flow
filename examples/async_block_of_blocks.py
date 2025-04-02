eaebd687010d633f1a6451e24ec1e9091b7b420a
AymenFJA <aymen.alsaadi@rutgers.edu>
2025-04-02 23:10:52 +0000

    block of blocks asyncio

--- examples/async_block_of_blocks.py
+++ examples/async_block_of_blocks.py
@@ -1,61 +1,63 @@
+import asyncio
 from radical.flow import WorkflowEngine, ResourceEngine, Task
 import time
 
-engine = ResourceEngine({'resource': 'local.localhost'})
-flow = WorkflowEngine(engine=engine)
+async def main():
+
+    engine = ResourceEngine({'resource': 'local.localhost'})
+    flow = WorkflowEngine(engine=engine)
 
 
-@flow
-def task1(*args):
-    return Task(executable='/bin/echo "I got executed at" && /bin/date') 
+    @flow
+    async def task1(*args):
+        return Task(executable='/bin/echo "I got executed at" && /bin/date') 
 
-@flow
-def task2(*args):
-    return Task(executable='/bin/echo "I got executed at" && /bin/date')
-
-
-@flow.block
-def block1(*args):
-    t1 = task1()
-    t2 = task2(t1)
-    t2.result()
-    print(f'block1 done at {time.time()}')
-
-@flow.block
-def block2(*args):
-    t3 = task1()
-    t4 = task2(t3)
-    t4.result()
-    print(f'block2 done at {time.time()}')
+    @flow
+    async def task2(*args):
+        return Task(executable='/bin/echo "I got executed at" && /bin/date')
 
 
-@flow.block
-def block1_of_blocks(*args):
-    b1 = block1()
-    b2 = block2(b1)
-    b2.result()
-    print(f'block_of_blocks done at {time.time()}')
+    @flow.block
+    async def block1(*args):
+        t1 = await task1()
+        t2 = await task2(t1)
+        await t2
+        print(f'block1 done at {time.time()}')
+
+    @flow.block
+    async def block2(*args):
+        t3 = await task1()
+        t4 = await task2(t3)
+        await t4
+        print(f'block2 done at {time.time()}')
 
 
-@flow.block
-def block2_of_blocks(*args):
-    b1 = block1()
-    b2 = block2(b1)
-    b2.result()
-    print(f'block_of_blocks done at {time.time()}')
+    @flow.block
+    async def block1_of_blocks(*args):
+        b1 = await block1()
+        b2 = await block2(b1)
+        await b2
+        print(f'block_of_blocks done at {time.time()}')
 
-@flow.as_async
-def run_blocks():
-    bob1 = block1_of_blocks()
-    bob2 = block2_of_blocks(bob1)
-    bob2.result()
 
-blocks = []
+    @flow.block
+    async def block2_of_blocks(*args):
+        b1 = await block1()
+        b2 = await block2(b1)
+        await b2
+        print(f'block_of_blocks done at {time.time()}')
 
-for block in range(10):
-    bob_fut = run_blocks()
-    blocks.append(bob_fut)
+    async def run_block_of_blocks(i):
+        bob1 = await block1_of_blocks()
+        bob2 = await block2_of_blocks(bob1)
+        await bob2
+        print(f'Block of block-{i} is finished')
 
-[b.result() for b in blocks]
+    results = await asyncio.gather(*[run_block_of_blocks(i) for i in range(1)])
 
-engine.shutdown()
+    engine.shutdown()
+
+
+if __name__ == '__main__':
+    asyncio.run(main())
+
