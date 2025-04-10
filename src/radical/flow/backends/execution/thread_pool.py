fe71273fc8bf3612eccecf9aa8ae30a6e4d8628e
AymenFJA <aymen.alsaadi@rutgers.edu>
2025-04-10 03:51:51 +0000

    This commit adds: 1-ThreadPool execution backend 2-ProcessPool execution backend 3-Deomnstrate the async/adapt isolation from the execution engine

---
+++ src/radical/flow/backends/execution/thread_pool.py
@@ -0,0 +1,57 @@
+import os
+import subprocess
+import typeguard
+from typing import Dict, Callable
+from concurrent.futures import ThreadPoolExecutor, Future
+import radical.pilot as rp
+import radical.utils as ru
+
+class Session():
+    def __init__(self):
+        self.path = os.getcwd()
+
+class TaskManager(ThreadPoolExecutor):
+    def __init__(self):
+        super().__init__()
+        self._callback_func: Callable[[Future], None] = lambda f: None  # default no-op
+
+    def submit_tasks(self, tasks: list):
+        for task in tasks:
+            # Submit task to thread pool
+            fut = self.submit(self._task_wrapper, task)
+            fut.add_done_callback(lambda f, task=task: self._callback_func(*f.result()))
+
+    def register_callback(self, func: Callable[[Future], None]):
+        self._callback_func = func
+
+    def _task_wrapper(self, task):
+        exec_list = [task['executable']]
+        exec_list.extend(task.get('arguments', []))  # fixed extend usage
+
+        result = subprocess.run(exec_list, capture_output=True, text=True, shell=True)
+
+        # Custom result logic
+        task['stdout'] = result.stdout
+        task['stderr'] = result.stderr
+        task['exit_code'] = result.returncode
+
+        state = 'DONE' if result.returncode == 0 else 'FAILED'
+        return task, state
+
+
+class ResourceEngine:
+
+    @typeguard.typechecked
+    def __init__(self, resources: Dict) -> None:
+        self._session = Session()
+        self.task_manager = TaskManager()
+
+    def state(self):
+        raise NotImplementedError
+
+    def task_state_cb(self, task, state):
+        raise NotImplementedError
+
+    def shutdown(self) -> None:
+        self.task_manager.shutdown(cancel_futures=True)
+        print('Shutdown is triggered, terminating the resources gracefully')
